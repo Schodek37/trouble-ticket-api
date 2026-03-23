bd030803dab15deeb4f034482d0a8c4d8d93d86a
Schodek <schodek987@gmail.com>
2026-03-23 18:34:12 +0100

    Initial project setup

---
+++ src/test/java/pl/netia/trouble_ticket_api/TroubleTicketApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.netia.trouble_ticket_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TroubleTicketApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
